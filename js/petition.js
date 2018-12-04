633de5ee4e002f79ec873e73dc17b5943f8ce13d
Ken Mickles <ken@37i.net>
2018-12-04 15:26:49 -0500

    create static site from lovescensorship source

---
+++ js/petition.js
@@ -0,0 +1,55 @@
+'use strict';
+
+(function() {
+  var petition = document.getElementById('petition');
+  var message = petition.querySelector('.message');
+  var form = petition.getElementsByTagName('form')[0];
+  var thanks = document.getElementById('thanks');
+
+  function flash(msg) {
+    function setMessage() {
+      message.removeEventListener(message, setMessage);
+      message.textContent = msg;
+      message.classList.remove('hidden');
+      message.classList.remove('invisible');
+    }
+
+    if (!message.classList.contains('invisible')) {
+      message.addEventListener('transitionend', setMessage);
+      message.classList.add('invisible');
+    } else {
+      setMessage();
+    }
+  }
+
+  function onError(e) {
+    flash('There was an issue submitting the form, please try again.');
+
+    // Report XHR errors or HTTP error codes to Sentry
+    if (typeof Raven !== 'undefined') Raven.captureException(e);
+  }
+
+  function onLoad(e) {
+    if (e && 200 <= e.code < 300) {
+      thanks.classList.remove('hidden');
+      thanks.classList.remove('invisible');
+
+      // Track successful form submissions
+      fbq('track', 'CompleteRegistration');
+    } else {
+      onError(e);
+    }
+  }
+
+  form.addEventListener('submit', function onSubmit(e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('error', onError);
+    xhr.addEventListener('load', onLoad);
+
+    xhr.open(form.getAttribute('method'), form.getAttribute('action'), true);
+    xhr.send(new FormData(form));
+  });
+})();
